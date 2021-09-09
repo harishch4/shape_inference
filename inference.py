7c47cec7ad91c1b1bd3baf896c51af68d3ac1b10
harish <harish@morphing.in>
2021-09-09 10:48:26 +0530

    print dims

--- inference.py
+++ inference.py
@@ -8,11 +8,15 @@ tensor = onnx.TensorProto()
 with open("input_0.pb", 'rb') as f:
     tensor.ParseFromString(f.read())
 g_input = model.graph.input[0]
+print(g_input.type.tensor_type.shape.dim)
 g_input.type.tensor_type.shape.dim[0].dim_value = tensor.dims[0]
 g_input.type.tensor_type.shape.dim[1].dim_value = tensor.dims[1]
+print(g_input.type.tensor_type.shape.dim)
 
+# shape inference
 inferred_model = shape_inference.infer_shapes(model)
 
+# check for dynamic shapes
 for value in inferred_model.graph.value_info:
     for dim in value.type.tensor_type.shape.dim:
         if dim.dim_param:
